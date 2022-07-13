d09726c08560fb9e256885df08e1467acadffb09
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-13 19:49:20 +0100

    Create first version of function playRound

--- script.js
+++ script.js
@@ -3,4 +3,42 @@ function computerPlay() {
     return handGesture[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerPlay());
+// Create function that takes two parameters - playerSelectio, computerSelection
+// Add two variables that will have value of parameters in lower case
+// Create nested ifs 
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelect = playerSelection.toLowerCase();
+    let computerSelect = computerSelection.toLowerCase();
+
+    if (playerSelect === "rock") {
+        if (computerSelect === "rock") {
+            return "A draw!";
+        } else if (computerSelect === "paper") {
+            return "You lose! Paper beats Rock.";
+        } else if (computerSelect === "scissors") {
+            return "You win! Rock beats Scissors.";
+        }
+    } else if (playerSelect === "paper") {
+        if (computerSelect === "rock") {
+            return "You win! Paper beats Rock.";
+        } else if (computerSelect === "paper") {
+            return "A draw!";
+        } else if (computerSelect === "scissors") {
+            return "You lose! Scissors beats Paper.";
+        }
+    } else if (playerSelect === "scissors") {
+        if (computerSelect === "rock") {
+            return "You lose! Rock beats Scissors.";
+        } else if (computerSelect === "paper") {
+            return "You win! Scissors beats Paper.";
+        } else if (computerSelect === "scissors") {
+            return "A draw!";
+        }
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
