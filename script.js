32c7eff6a53b9d6bdb7da9d896db4bab3aeadcd6
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-15 15:18:13 +0100

    Add comments about what each function does

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-function computerPlay() {
-    const handGesture = ["rock", "paper", "scissors"];
-    return handGesture[Math.floor(Math.random() * 3)];
-}
-
-function playerPlay() {
-    return prompt("Rock, paper or scissors?").toLowerCase();
-}
-
-function playRound(playerSelection, computerSelection) {
-    let playerSelect = playerSelection.toLowerCase();
-    let computerSelect = computerSelection.toLowerCase();
-
-    if (playerSelect === computerSelect) {
-        return "A draw! ";
-    } else if ((playerSelect === "rock" && computerSelect === "scissors") ||
-        (playerSelect === "paper" && computerSelect === "rock") ||
-        (playerSelect === "scissors" && computerSelect === "paper")) {
-        return "You win!";
-    } else if ((playerSelect === "rock" && computerSelect === "paper") ||
-        (playerSelect === "paper" && computerSelect === "scissors") ||
-        (playerSelect === "scissors" && computerSelect === "rock")) {
-        return "You lose!";
-    }
-}
+/* Main function that starts, contain information about scores and display output */
 
 function start() {
     let playerScore = 0;
@@ -44,6 +20,40 @@ function start() {
     }
     console.log(declareWinner(playerScore, computerScore));
 }
+
+/* Choose random hand gesture for computer */
+
+function computerPlay() {
+    const handGesture = ["rock", "paper", "scissors"];
+    return handGesture[Math.floor(Math.random() * 3)];
+}
+
+/* Ask user to choose hand gesture */
+
+function playerPlay() {
+    return prompt("Rock, paper or scissors?").toLowerCase();
+}
+
+/* Function generates messages depenging on who win each round */
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelect = playerSelection.toLowerCase();
+    let computerSelect = computerSelection.toLowerCase();
+
+    if (playerSelect === computerSelect) {
+        return "A draw! ";
+    } else if ((playerSelect === "rock" && computerSelect === "scissors") ||
+        (playerSelect === "paper" && computerSelect === "rock") ||
+        (playerSelect === "scissors" && computerSelect === "paper")) {
+        return "You win!";
+    } else if ((playerSelect === "rock" && computerSelect === "paper") ||
+        (playerSelect === "paper" && computerSelect === "scissors") ||
+        (playerSelect === "scissors" && computerSelect === "rock")) {
+        return "You lose!";
+    }
+}
+
+/* Function that generates result message based on who win */
 
 function declareWinner(playerScore, computerScore) {
     let result = `Player score: ${playerScore}. Computer score: ${computerScore} `;
