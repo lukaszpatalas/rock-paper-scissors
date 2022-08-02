762ae58a1fdb5ff115d9d4550ff4e6696c1664ed
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-08-02 21:00:29 +0100

    Simplify logic for computer selection

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
-
-
 function game() {
-
     const buttons = document.querySelectorAll(".player-select > .game-select");
-
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             if (playerScore < 5 && computerScore < 5) {
@@ -14,54 +10,30 @@ function game() {
         });
     });
 }
-
 function computerSelect() {
     const handGesture = ["jet", "gun", "bomb"];
     const computerChoice = handGesture[Math.floor(Math.random() * 3)];
-
     const computerIcons = document.querySelectorAll(".enemy-select .choice-icon");
 
-    // Try alter this code with Node.parentNode
-
-/*     computerIcons.forEach((icon) => {
-        icon.style.color = "#dadada";
-        icon.style.borderColor = "#747474";
-        if (icon.classList.contains("fa-jet-fighter")) {
-            icon.style.color = "#ff652f";
-            icon.style.borderColor = "#ff652f";
-        } else if (icon.classList.contains("fa-gun")) {
-            icon.style.color = "#ffe400";
-            icon.style.borderColor = "#ffe400";
-        } else if (icon.classList.contains("fa-bomb")) {
-            icon.style.color = "#14a76c";
-            icon.style.borderColor = "#14a76c";
+    computerIcons.forEach((icon) => {
+        if (icon.parentNode.classList.contains(computerChoice)) {
+            if (computerChoice === "jet") {
+                icon.style.color = "#ff652f";
+                icon.style.borderColor = "#ff652f";
+            } else if (computerChoice === "gun") {
+                icon.style.color = "#ffe400";
+                icon.style.borderColor = "#ffe400";
+            } else if (computerChoice === "bomb") {
+                icon.style.color = "#14a76c";
+                icon.style.borderColor = "#14a76c";
+            }
+        } else if (!icon.parentNode.classList.contains(computerChoice)) {
+            icon.style.color = "#dadada";
+            icon.style.borderColor = "#747474";
         }
-    }) */
-    if (computerChoice === "jet") {
-        computerIcons[0].style.color = "#ff652f";
-        computerIcons[0].style.borderColor = "#ff652f";
-        computerIcons[1].style.color = "#dadada";
-        computerIcons[1].style.borderColor = "#747474";
-        computerIcons[2].style.color = "#dadada";
-        computerIcons[2].style.borderColor = "#747474";
-    } else if (computerChoice === "gun") {
-        computerIcons[0].style.color = "#dadada";
-        computerIcons[0].style.borderColor = "#747474";
-        computerIcons[1].style.color = "#ffe400";
-        computerIcons[1].style.borderColor = "#ffe400";
-        computerIcons[2].style.color = "#dadada";
-        computerIcons[2].style.borderColor = "#747474";
-    } else {
-        computerIcons[0].style.color = "#dadada";
-        computerIcons[0].style.borderColor = "#747474";
-        computerIcons[1].style.color = "#dadada";
-        computerIcons[1].style.borderColor = "#747474";
-        computerIcons[2].style.color = "#14a76c";
-        computerIcons[2].style.borderColor = "#14a76c";
-    }
+    })
     return computerChoice;
 }
-
 function playerSelect(button) {
     const playerIcons = document.querySelectorAll(".player-select .choice-icon");
     if (button.classList.contains("jet")) {
@@ -78,7 +50,7 @@ function playerSelect(button) {
         playerIcons[1].style.borderColor = "#ffe400";
         playerIcons[2].style.color = "#dadada";
         playerIcons[2].style.borderColor = "#747474";
-    } else {
+    } else if (button.classList.contains("bomb")) {
         playerIcons[0].style.color = "#dadada";
         playerIcons[0].style.borderColor = "#747474";
         playerIcons[1].style.color = "#dadada";
@@ -88,15 +60,15 @@ function playerSelect(button) {
     }
     return button.id;
 }
-
 function playRound(playerSelection, computerSelection) {
     const playerResult = document.querySelector("#player-score");
     const computerResult = document.querySelector("#computer-score");
     const roundInfo = document.querySelector("#round-info");
     const roundMessage = document.querySelector("#round-message")
-
     let playerChoice = playerSelection[0].toUpperCase() + playerSelection.slice(1);
     let computerChoice = computerSelection[0].toUpperCase() + computerSelection.slice(1);
+
+    console.log(`Player choice: ${playerChoice}. Computer choice: ${computerChoice}.`)
 
     if (playerSelection === computerSelection) {
         roundInfo.textContent = "A draw!";
@@ -116,10 +88,12 @@ function playRound(playerSelection, computerSelection) {
         roundInfo.textContent = "You lose!";
         roundMessage.textContent = `${computerChoice} beats ${playerChoice}`;
     }
+    console.log(`Player score: ${playerScore}. Computer score: ${computerScore}`);
 
     // Extra check as scores can be updated in previous ifs
     if (playerScore === 5 || computerScore === 5) {
         roundInfo.textContent = declareWinner(playerScore, computerScore);
+        console.log(declareWinner(playerScore, computerScore));
     }
 }
 
@@ -136,5 +110,4 @@ function declareWinner(playerScore, computerScore) {
         return result;
     }
 }
-
 game();
